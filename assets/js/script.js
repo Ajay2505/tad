b4bc52b131def9960efb04516d5b944d5f77dc61
sudhamsh99 <92301797+sudhamsh99@users.noreply.github.com>
2023-12-06 10:33:58 +0530

    sa

--- assets/js/script.js
+++ assets/js/script.js
@@ -142,3 +142,353 @@ jQuery(document).ready(function ($) {
     $(".swiper-progress-bar .progress").stop().parent().addClass("stopped");
   });
 });
+
+
+
+
+// gsap-start
+
+gsap.registerPlugin(ScrollTrigger);
+
+    gsap.from("header", {
+      opacity: 0,
+      duration: 2,
+    });
+
+    gsap.from(".solider1", {
+      y: 200,
+      x: -200,
+      duration: 0.5,
+      opacity: 0,
+      delay: 0.75
+    });
+
+    gsap.from(".hero_text", {
+      delay:1.5,
+      duration: 1,
+      opacity: 0,
+    });
+
+    gsap.from(".sec_1 .one", {
+      x: 200,
+      duration: 0.5,
+      opacity: 0,
+    });
+
+    gsap.from(".sec_1 .two", {
+      x: -200,
+      duration: 0.5,
+      opacity: 0,
+    });
+
+    gsap.from(".sec_1 .three", {
+      x: 200,
+      duration: 0.5,
+      opacity: 0,
+    });
+
+
+    const timeline = gsap.timeline({
+      scrollTrigger: {
+        trigger: ".horse1",
+        start: "top center",
+        end: "top top",
+        scrub: true,
+      },
+    });
+
+    timeline
+      .add(gsap.to(".horse1", { x: -130 }))
+      .add(gsap.to(".horse1", { y: 240 }));
+
+    
+    gsap.from(".sec2_ani1", {
+      x: -200,
+      duration: 0.5,
+      opacity: 0,
+      scrollTrigger: {
+        trigger: ".sec_2",
+        start: "top center",
+        end: "top top",
+      },
+    });
+
+    gsap.from(".sec2_ani2", {
+      x: 200,
+      duration: 0.5,
+      opacity: 0,
+      delay:1,
+      scrollTrigger: {
+        trigger: ".sec_2",
+        start: "top center",
+        end: "top top",
+      },
+    });
+
+    const elements = document.querySelectorAll(".ozerotoone");
+
+    elements.forEach((element, index) => {
+      gsap.from(element, {
+        duration: 1,
+        opacity: 0,
+        scrollTrigger: {
+          trigger: ".sec2_horse",
+          start: "bottom center",
+          end: "90% top",
+          scrub: true,
+          stagger: 0.2 * index, // Adjust the stagger value based on the index
+        },
+      });
+    });
+
+    const timeline2 = gsap.timeline({
+      scrollTrigger: {
+        trigger: ".horse2",
+        start: "top center",
+        end: "top top",
+        scrub: true,
+      },
+    });
+
+    timeline2
+      .add(gsap.to(".horse2", { x: 210 }))
+      .add(gsap.to(".horse2", { y: 110 }));
+
+
+    gsap.from(".horsetext", {
+      opacity: 0,
+      duration: 0.5,
+      opacity: 0,
+      delay:1,
+      scrollTrigger: {
+        trigger: ".horse2",
+        start: "top center",
+        end: "top top",
+        scrub: true
+      },
+    });
+
+    gsap.from(".sec3_heading", {
+      x: -200,
+      duration: 0.5,
+      opacity: 0,
+      scrollTrigger: {
+        trigger: ".sec_3",
+        start: "top center",
+        end: "top top",
+      },
+    });
+
+    gsap.from(".sec3_subheading", {
+      x: 200,
+      duration: 0.5,
+      opacity: 0,
+      scrollTrigger: {
+        trigger: ".sec_3",
+        start: "top center",
+        end: "top top",
+      },
+    });
+
+    gsap.from(".sec3_anipara", {
+      opacity: 0,
+      duration: 0.5,
+      opacity: 0,
+      delay: 0.75,
+      scrollTrigger: {
+        trigger: ".sec_3",
+        start: "top center",
+        end: "top top",
+      },
+    });
+
+
+    gsap.from(".sec4_heading1", {
+      x: -200,
+      duration: 0.5,
+      opacity: 0,
+      scrollTrigger: {
+        trigger: ".sec_4",
+        start: "top center",
+        end: "top top",
+      },
+    });
+
+    gsap.from(".sec4_heading2", {
+      x: 200,
+      duration: 0.5,
+      opacity: 0,
+      scrollTrigger: {
+        trigger: ".sec_4",
+        start: "top center",
+        end: "top top",
+      },
+    });
+
+    const subparani1 = document.querySelectorAll(".subparani1");
+
+    subparani1.forEach((element) => {
+      gsap.from(element, {
+        x: -200,
+        duration: 0.5,
+        opacity: 0,
+        scrollTrigger: {
+          trigger: element,
+          start: "top center",
+          end: "top top",
+        },
+      });
+    });
+
+    const subparani2 = document.querySelectorAll(".subparani2");
+
+    subparani2.forEach((element) => {
+      gsap.from(element, {
+        y: 200,
+        duration: 0.5,
+        opacity: 0,
+        scrollTrigger: {
+          trigger: element,
+          start: "top bottom",
+          end: "top top",
+        },
+      });
+    });
+
+
+
+    gsap.to(".elephant1", {
+      y: 310, 
+      scrollTrigger: {
+        trigger: ".elephant1",
+        start: "top center",
+        end: "top top",
+        scrub: true,
+      },
+    });
+
+    gsap.to(".solider2", {
+      y: 150, 
+      x: -160,
+      scrollTrigger: {
+        trigger: ".solider2",
+        start: "top center",
+        end: "top top",
+        scrub: true,
+      },
+    });
+
+    const timeline3 = gsap.timeline({
+      scrollTrigger: {
+        trigger: ".horse3",
+        start: "top center",
+        end: "top top",
+        scrub: true,
+      },
+    });
+
+    timeline3
+      .add(gsap.to(".horse3", { x: 130 }))
+      .add(gsap.to(".horse3", { y: 240 }));
+
+    const timeline4 = gsap.timeline({
+      scrollTrigger: {
+        trigger: ".horse4",
+        start: "top center",
+        end: "top top",
+        scrub: true,
+      },
+    });
+
+    timeline4
+      .add(gsap.to(".horse4", { x: 210 }))
+      .add(gsap.to(".horse4", { y: 110 }));
+
+
+    gsap.to(".solider3", {
+      y: 160, 
+      x: -160,
+      scrollTrigger: {
+        trigger: ".solider3",
+        start: "top center",
+        end: "top top",
+        scrub: true,
+      },
+    });
+
+    gsap.from(".sec6_heading1", {
+      x: -200,
+      duration: 0.5,
+      opacity: 0,
+      scrollTrigger: {
+        trigger: ".sec6_heading1",
+        start: "top center",
+        end: "top top",
+      },
+    });
+
+    gsap.from(".sec6_heading2", {
+      x: 200,
+      duration: 0.5,
+      opacity: 0,
+      scrollTrigger: {
+        trigger: ".sec6_heading1",
+        start: "top center",
+        end: "top top",
+      },
+    });
+
+    gsap.from(".sec7_heading1", {
+      x: -200,
+      duration: 0.5,
+      opacity: 0,
+      scrollTrigger: {
+        trigger: ".sec7_heading1",
+        start: "top center",
+        end: "top top",
+      },
+    });
+
+    gsap.from(".sec7_heading2", {
+      x: 200,
+      duration: 0.5,
+      opacity: 0,
+      scrollTrigger: {
+        trigger: ".sec7_heading1",
+        start: "top center",
+        end: "top top",
+      },
+    });
+
+    gsap.from(".sec7paraani", {
+      duration: 0.5,
+      opacity: 0,
+      delay: 0.75,
+      scrollTrigger: {
+        trigger: ".sec7paraani",
+        start: "top center",
+        end: "top top",
+      },
+    });
+
+    gsap.from(".sec8_heading1", {
+      x: -200,
+      duration: 0.5,
+      opacity: 0,
+      scrollTrigger: {
+        trigger: ".sec8_heading1",
+        start: "top center",
+        end: "top top",
+      },
+    });
+
+    gsap.from(".sec8_heading2", {
+      x: 200,
+      duration: 0.5,
+      opacity: 0,
+      scrollTrigger: {
+        trigger: ".sec8_heading1",
+        start: "top center",
+        end: "top top",
+      },
+    });
